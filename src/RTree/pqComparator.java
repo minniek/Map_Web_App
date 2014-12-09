1c3cedbb0b86ca36455e3f77d90caa7d657ddf98
minniek <minniek@bu.edu>
2014-12-09 12:03:49 -0500

    Update pqComparator.java

--- src/RTree/pqComparator.java
+++ src/RTree/pqComparator.java
@@ -6,12 +6,12 @@ public class pqComparator implements Comparator<Double>
 {
 	public int compare(Double arg0, Double arg1) {
 		if (arg1 > arg0) {
-            return -1;
-        }
+			return -1;
+        	}
         
-		if ( arg1  < arg0 ) {
-            return 1;
-        }
+		if ( arg1 < arg0 ) {
+            		return 1;
+        	}
         
 		return 0;
 	}

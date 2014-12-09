39edc442d5ac8759bb562150f50cfe04c488be52
minniek <minniek@bu.edu>
2014-12-09 11:17:43 -0500

    Update FindNearestServlet.java

--- src/EC504/GUI/FindNearestServlet.java
+++ src/EC504/GUI/FindNearestServlet.java
@@ -23,8 +23,7 @@ public class FindNearestServlet extends HttpServlet {
 	private static final long serialVersionUID = 102831973239L;
       
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-			// Store form fields and convert to variable type double
+			// Store form fields and convert to appropriate data types
 			Double y = Double.parseDouble(request.getParameter("latitude"));
 			Double x = Double.parseDouble(request.getParameter("longitude"));
 			int k = Integer.parseInt(request.getParameter("k"));
@@ -33,91 +32,89 @@ public class FindNearestServlet extends HttpServlet {
 			x = (long) (x * 10000) / 10000.0;
 			y = (long) (y * 10000) / 10000.0;
 	 
-	        // Load map data
+	        	// Load map data
 			String myFile = getServletContext().getRealPath("NationalFile_StateProvinceDecimalLatLong.txt");
 			HashMap mapData_States = LoadMapData(myFile);
 
-			// Create RTree
+			// Create RTree(ish) data structure
 			RTreeNode_GlobalScale Root = CreateRTree(mapData_States);
 			ArrayList<RTreeNode_GlobalScale> nodesContainingPoint;
 			nodesContainingPoint = Root.findNodesContainingPoint(x, y);
 			String stateAbbrv = null;
 			for (int ii = 0; ii < nodesContainingPoint.size(); ii++) {
-				RTreeNode_GlobalScale currentStateNode = nodesContainingPoint.get(ii);			// Get current state node
+				// Get current state node
+				RTreeNode_GlobalScale currentStateNode = nodesContainingPoint.get(ii); 
 				//if (stateAbbrv.equals("Root: United States")) continue;
-				stateAbbrv = currentStateNode.getName();										// Get name of state (abbreviation)	
+				stateAbbrv = currentStateNode.getName(); // Get name of state (abbreviation)	
 				//System.out.println(nodesContainingPoint.get(ii));
 			}
 			
-			// Display resulting nodes that point is in or nearby
+			// Display resulting nodes that contain the original coordinates or nearby
 			for (int ii = 0; ii < nodesContainingPoint.size(); ii++) {
 				System.out.println("From recursive function. Nodes containing test point: " + nodesContainingPoint.get(ii).getName());
 			}
 		
 			// Try priority queue
 			//pqDistances pq = new pqDistances(mapData_States.States, 100, "CO", -104.98, 39.7516667);
-			pqDistances pq = new pqDistances(mapData_States, 100, stateAbbrv, x, y, k); // 
+			pqDistances pq = new pqDistances(mapData_States, 100, stateAbbrv, x, y, k);
 			//pq.printQueue(k);
 			
-	        // Get response writer
-	        PrintWriter writer = response.getWriter();
-	        // Build HTML code
-	        String htmlResponse = "<html>";
-	        htmlResponse += "<style type=\"text/css\"> body { font-size: 0.8em; font-family: sans-serif; margin-top: 0.1em; margin-left: 0; margin-right: 0;} </style>";
-	        htmlResponse += "<body>Original point was: " + y + ", " + x + "<br/>";      
-	        htmlResponse += "The " + k + " nearest counties are: " + "<br/>";
-	        htmlResponse += pq.printQueue(k);
-	        htmlResponse += "</body>";
-	        htmlResponse += "</html>";
-	      
-	        // Return response
-	        writer.println(htmlResponse);
+		        // HTML Response
+		        PrintWriter writer = response.getWriter();
+		        String htmlResponse = "<html>";
+		        htmlResponse += "<style type=\"text/css\"> body { font-size: 0.8em; font-family: sans-serif; margin-top: 0.1em; margin-left: 0; margin-right: 0;} </style>";
+		        htmlResponse += "<body>Original point was: " + y + ", " + x + "<br/>";      
+		        htmlResponse += "The " + k + " nearest counties are: " + "<br/>";
+		        htmlResponse += pq.printQueue(k);
+		        htmlResponse += "</body>";
+		        htmlResponse += "</html>";
+		        // Return response
+		        writer.println(htmlResponse);
 }
 
 	public static RTreeNode_GlobalScale CreateRTree(HashMap<String, HashMap<String, ArrayList>> mapData) {
-		RTreeNode_GlobalScale rootNode = new RTreeNode_GlobalScale();			// Root of tree
+		// Root of treec
+		RTreeNode_GlobalScale rootNode = new RTreeNode_GlobalScale();			
 		
-		//This prints out all the states. Just for debugging purposes.
-	    for (Object current_state : mapData.keySet()) {											// Iterate through all states
+		// This prints out all the states (for debugging purposes)
+		for (Object current_state : mapData.keySet()) { // Iterate through all states
+	    		// IMPORTANT: Uncomment when selecting to print out a specific state (for testing purposes only)
+	    		//if (!current_state.toString().equals("MH")) continue;
 	    	
-	    	// IMPORTANT: Uncomment when selecting to print out a specific state (for testing purposes only)
-	    	//if (!current_state.toString().equals("MH")) continue;
-	    	
-	    	RTreeNode_GlobalScale stateNode = new RTreeNode_GlobalScale();						// Initialize current state's nodes
-	        //System.out.println(current_state);													// Prints out state
-	        stateNode.setName(current_state.toString());										// Add state's name to node
+	    		RTreeNode_GlobalScale stateNode = new RTreeNode_GlobalScale(); // Initialize current state's nodes
+	        	//System.out.println(current_state); // Prints out state
+	        	stateNode.setName(current_state.toString()); // Add state's name to node
 	        
-	        HashMap state_value = (HashMap) mapData.get(current_state);							// Get internal state hashmap (counties and their dimensions)
+			HashMap state_value = (HashMap) mapData.get(current_state); // Get internal state hashmap (counties and their dimensions)
 	        
-	        for (Object current_county : state_value.keySet()){									// Iterate through all counties in state
-	        	
-	            //System.out.print("   - " + current_county);										// Prints out county belonging to [this] state
-	            ArrayList county_dimensions = (ArrayList) state_value.get(current_county);		// Get county's list of rectangular dimensions.
-	            //System.out.print(" " + county_dimensions + "\n");								// Print out county's dimensions (points form)
-	            
-	            // Store coordinates. Note ArrayList points order: [x1, y1, x2, y2]
-	            Double x1, x2, y1, y2;
-	            
-	            if (county_dimensions.size() != 4) continue;		// If ArrayList does not have 4 points, skip county
-	            
-	            y2 = (Double) county_dimensions.get(0);			// Get y2 = max latitude
-	            x1 = (Double) county_dimensions.get(1);			// Get x1 = min longitude 
-	            y1 = (Double) county_dimensions.get(2);			// Get y1 = min latitude
-	            x2 = (Double) county_dimensions.get(3);			// Get x2 = max longitude
-	            
-	            // Create county node: name, x1, x2, y1, y2
-	            RTreeNode_GlobalScale countyNode = new RTreeNode_GlobalScale(current_county.toString(), x1, x2, y1, y2);
-	            
-	            // Add county to state
-	            stateNode.addChild(countyNode);
-	        }
-	        
-	        // Add state to root node
-	        rootNode.addChild(stateNode);
-	    }
-	    
+	        	// Iterate through all counties in state
+	        	for (Object current_county : state_value.keySet()) { 
+				//System.out.print("   - " + current_county); // Prints out county belonging to [this] state
+				// Get county's list of rectangular dimensions
+				ArrayList county_dimensions = (ArrayList) state_value.get(current_county); 
+				//System.out.print(" " + county_dimensions + "\n"); // Print out county's dimensions (points form)
+				
+				// Store coordinates. Note ArrayList points order: [x1, y1, x2, y2]
+				Double x1, x2, y1, y2;
+				
+				// If ArrayList does not have 4 points, skip county
+				if (county_dimensions.size() != 4) continue; 
+				
+				y2 = (Double) county_dimensions.get(0); // Get y2 = max latitude
+				x1 = (Double) county_dimensions.get(1); // Get x1 = min longitude 
+				y1 = (Double) county_dimensions.get(2); // Get y1 = min latitude
+				x2 = (Double) county_dimensions.get(3); // Get x2 = max longitude
+				
+				// Create county node: name, x1, x2, y1, y2
+				RTreeNode_GlobalScale countyNode = new RTreeNode_GlobalScale(current_county.toString(), x1, x2, y1, y2);
+				
+				// Add county to state
+				stateNode.addChild(countyNode);
+	        	}
+		        // Add state to root node
+		        rootNode.addChild(stateNode);
+	    	}
 	    rootNode.setName("Root: United States");
-		
 	    return rootNode;
 	}
 

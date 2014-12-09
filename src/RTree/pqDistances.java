a9b5b0aabf8ca0c02f609867acf333df185a4d5a
minniek <minniek@bu.edu>
2014-12-09 12:06:22 -0500

    Update pqDistances.java

--- src/RTree/pqDistances.java
+++ src/RTree/pqDistances.java
@@ -10,7 +10,7 @@ public class pqDistances
 {
     private Comparator<Double> comparator; // used to "sort" prioriy queue
     private PriorityQueue<Double> queue; // stores distances only
-    private HashMap<Double,String> distance_mapping; //maps distances to the distances in the priority queue "queue"
+    private HashMap<Double,String> distance_mapping; // maps distances to the distances in the priority queue "queue"
     private int size; // Max. size of priority queue
     private HashMap<String, HashMap<String, ArrayList>> States; // key ---> State, value ---> hashmap containing another hashmap of county (key) and the coordinates (value)
 
@@ -21,13 +21,13 @@ public class pqDistances
        @param longitude = origin point (y)
     */
     
-    public pqDistances (HashMap states, int size, String state, double longitude, double latitude, int k){
+    public pqDistances (HashMap states, int size, String state, double longitude, double latitude, int k) {
     	this.comparator = new pqComparator();
         this.queue = new PriorityQueue<Double>(size, this.comparator);
         this.distance_mapping = new HashMap<Double,String>();
         this.size = size;
 
-         //Find state in hashmap
+        // Find state in hashmap
         HashMap<String, ArrayList> Counties = (HashMap)states.get(state);
         String county_name;
         ArrayList<Double> county_temp; //current county in the for loop
@@ -35,26 +35,25 @@ public class pqDistances
         double distance;
 
         int count = 0;
-        for (Object county : Counties.keySet()){
-        	if (county == null) continue;
+        for (Object county : Counties.keySet()) {
+            if (county == null) continue;
             county_name = (String)county;
             
-            county_temp = (ArrayList)Counties.get(county_name); //the ArrayList for the current county (contains MBR data)
+            county_temp = (ArrayList)Counties.get(county_name); // the ArrayList for the current county (contains MBR data)
             
-            //Find center point of the county
+            // Find center point of the county
             center_point = getCenter(county_temp.get(0),county_temp.get(1),county_temp.get(2),county_temp.get(3));
-            
-            
+           
             //System.out.println(county_name + ": " + center_point[0] + ", " + center_point[1]);
             
-            //Calculate distance
+            // Calculate distance
             distance = calculateDistance(center_point, longitude, latitude);
             
-            //Insert into the queue
+            // Insert into the queue
             //addDistanceToQueue(distance);
             queue.add(distance);
             
-            //Insert into hashmap to create a mapping between "queue" and "Counties"
+            // Insert into hashmap to create a mapping between "queue" and "Counties"
             distance_mapping.put(distance,county_name);
             count++;
         }
@@ -72,29 +71,29 @@ public class pqDistances
     }
 
     // Get coordinates for the center of the county
-    public double[] getCenter(double x1, double y1, double x2, double y2){
+    public double[] getCenter(double x1, double y1, double x2, double y2) {
         double mid_x = (x1 + x2) / 2;
         double mid_y = (y1 + y2) / 2;
         double[] center = { mid_x , mid_y };
         return center;
     }
 
-    //Get distance
-    public double calculateDistance(double[] center, double longitude, double latitude){
+    // Get distance
+    public double calculateDistance(double[] center, double longitude, double latitude) {
         double x = (latitude - center[0]); //* Math.cos((longitude + center[1])/2);
         double y = (longitude - center[1]);
         return (Math.sqrt(x*x + y*y));
     }
 
-    //Add distance to queue
-    private void addDistanceToQueue(double distance){
+    // Add distance to queue
+    private void addDistanceToQueue(double distance) {
     	//queue = new PriorityQueue<Double>(size, comparator);
     	queue.add(distance);
     }
 
     //Print top 'k' counties from the queue
-    public String printQueue(int k){
-         //PriorityQueue<Double> temp_queue = new PriorityQueue<Double>(size, comparator);
+    public String printQueue(int k) {
+         // PriorityQueue<Double> temp_queue = new PriorityQueue<Double>(size, comparator);
          double d;
          String county_name;
          int counter = 0;
@@ -102,12 +101,12 @@ public class pqDistances
          while ((queue.size() != 0) && (counter < k) ){
               d = queue.remove();
              
-              //Now find the distance 'd' in the hashmap to figure out which county 'distance' corresponds to
+              // Now find the distance 'd' in the hashmap to figure out which county 'distance' corresponds to
               county_name = (String)distance_mapping.get(d);
               county_results += (county_name + " [distance: " + String.format("%.8g", d) + " km] </br>");
               System.out.println("County: " + county_name + " [distance: " + String.format("%.8g", d) + " km]");
               counter++;
          }
-		return county_results;   
+	return county_results;   
     }
 }
